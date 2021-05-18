65e1fc338e475e27a19225c7e8100846d82cae58
ytsmm <82078728+ytsmm@users.noreply.github.com>
2021-05-19 00:23:23 +0500

    Add files via upload

--- answer.py
+++ answer.py
@@ -1,18 +1,13 @@
-import gensim
+import training
 
-w2v_fpath = "all.norm-sz100-w10-cb0-it1-min100.w2v"
-w2v = gensim.models.KeyedVectors.load_word2vec_format(w2v_fpath, binary=True, unicode_errors='ignore')
-w2v.init_sims(replace=True)
-
-
-for word, score in w2v.most_similar(u"солнце"):
-    print(word, score)
-
-def vec(a):
-    for i in range(len(a)):
-        for token in a[i]:
-            print(token, w2v.get_vector(token))
-            print(w2v.most_similar(token))
+def answer(text):
+    response = ''
+    for i in range(len(text)):
+        s = ''
+        for word in text[i]:
+            s = str(word) + ' '
+        response = response + str(training.chatty.get_response(s)) + '\n'
+    return response
 
 
-import keras
+
