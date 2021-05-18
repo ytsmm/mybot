86098fb6b33b7d21d80c1a4750e80cc94e531be5
ytsmm <82078728+ytsmm@users.noreply.github.com>
2021-05-19 00:43:11 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import morph_analyzer
 import vectorizer
-import training
+import answer
 
-# Входной текст: Привет, бот! Как твои дела?
-raw = input("Здравствуйте! Введите текст: ")
-text = morph_analyzer.morph(raw)
-print(text)
+# Доброго дня! Я рад видеть тебя. Как твои дела?
+# Расскажи мне, что значит чат-бот?
+# 123 -*#% сонце малако ()
+while True:
+    test = input("Если хотите начать диалог, нажмите любую кнопку, иначе - 0: ")
+    if test != '0':
+        raw = input("Приветствую, я чат-бот! Поговорим?\n")
 
-vector_text = vectorizer.vectorizer(text)
-print(vector_text)
+        morph_text = morph_analyzer.morph(raw)
+        print(morph_text)
 
-answer = training.greet(vector_text)
-print(answer)
+        vector_text = vectorizer.vectorizer(morph_text)
+        print(vector_text)
+
+        response = answer.answer(morph_text)
+        print(response)
+    else:
+        print("До свидания!")
+        break
